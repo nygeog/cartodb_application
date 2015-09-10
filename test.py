fd77b7438457c62218ee4b033d8ae1b536620a1b
nygeog <daniel.martin.sheehan@gmail.com>
2015-09-10 13:53:46 -0400

    update

--- test.py
+++ test.py
@@ -3,5 +3,5 @@ from cartodbAWCCensus import cartodbAWCensus
 cartoDBusername = 'dms8md23'
 cartoDBapikey = 'fc4b0fe709cc086fd177768e648694d6be3170dc'
 
-cartodbAWCensus(    '40.7127','-74.0059','2500','nyct2010',    cartoDBusername, cartoDBapikey)
-#def cartodbAWCensus(inLat,   inLng,     bufDist,censusFeature,username,        apikey):
+cartodbAWCensus('40.7127','-74.0059','2500','nyct2010',    cartoDBusername, cartoDBapikey)
+#cartodbAWCensus(inLat,   inLng,     bufDist,censusFeature,username,        apikey):
